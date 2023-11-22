20beefde597699bcd2737f1a534a7694f8753c06
22JH <toitoii080@gmail.com>
2023-11-22 21:57:31 +0900

    사용하지 않는 타입 제거 (types->category.type.ts : MultiValue)

--- app/types/category.type.ts
+++ app/types/category.type.ts
@@ -1,10 +1,6 @@
-import type { MultiValue } from "/Users/a123/myblog/node_modules/react-select/dist/declarations/src/types.d.ts";
-
 export interface SelectOption {
   _id?: string;
   label: string;
   value: string;
   count?: number;
 }
-
-export type MultiOption = MultiValue<SelectOption>;
