f2f953a70d15b99056b3d119306fd09c9b0f23d0
22JH <toitoii080@gmail.com>
2023-11-24 00:36:48 +0900

    [FE] UPDATE : FCP 원인 찾기 3 - layout에서의 db호출

--- app/(routes)/post/[category]/page.tsx
+++ app/(routes)/post/[category]/page.tsx
@@ -9,54 +9,45 @@ interface PropType {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-// export const generateMetadata = async ({
-//   params,
-// }: PropType): Promise<Metadata> => {
-//   const category = params.category;
-//   return {
-//     category,
-//   };
-// };
+export const generateMetadata = async ({
+  params,
+}: PropType): Promise<Metadata> => {
+  const category = params.category;
+  return {
+    category,
+  };
+};
 
 export default async function Post({ params, searchParams }: PropType) {
-  // const page = Number(searchParams.page as string);
-  // console.log("------------------------------------------------");
+  const page = Number(searchParams.page as string);
   // const { posts, totalPage } = await getPostByCategory(
   //   params.category,
   //   Number(page)
   // );
-  // const decodedCategory = decodeURI(params.category);
+  const decodedCategory = decodeURI(params.category);
   // const { posts, totalPage } = await getPostByCategory(
   //   params.category,
   //   Number(page)
   // );
-  // const startTime = Date.now();
 
-  // const res = await fetch(
-  //   `https://www.joo-dev.com/api/post/${params.category}?page=${page}`,
-  //   { cache: "no-store" }
-  // );
-  // const endTime = Date.now();
-  // const fetchTime = endTime - startTime;
+  const res = await fetch(
+    `https://www.joo-dev.com/api/post/${params.category}?page=${page}`,
+    { cache: "no-store" }
+  );
 
-  // console.log(
-  //   `Fetch time: ${fetchTime}ms`,
-  //   "------------------------------------------"
-  // );
+  const data = await res.json();
+  const posts = data.posts;
 
-  // const data = await res.json();
-  // const posts = data.posts;
-
-  // const totalPage = data.totalPage;
+  const totalPage = data.totalPage;
 
   return (
     <>
-      {/* <PostList posts={posts} category={decodedCategory} /> */}
-      {/* <PaginationBar
+      <PostList posts={posts} category={decodedCategory} />
+      <PaginationBar
         page={page}
         totalPage={totalPage}
         category={params.category}
-      /> */}
+      />
     </>
   );
 }
--- app/(routes)/post/layout.tsx
+++ app/(routes)/post/layout.tsx
@@ -1,19 +1,19 @@
-import { getCategory } from "@/app/lib/actions/category.action";
-import * as styles from "./postLayout.css";
-import { getAllPost } from "@/app/lib/actions/post.actions";
-import CategoryBar from "@/app/components/post/CategoryBar";
+// import { getCategory } from "@/app/lib/actions/category.action";
+// import * as styles from "./postLayout.css";
+// import { getAllPost } from "@/app/lib/actions/post.actions";
+// import CategoryBar from "@/app/components/post/CategoryBar";
 import BackBtn from "@/app/components/svg/BackBtn";
 
 const postListLayout = async ({ children }: { children: React.ReactNode }) => {
-  const categories = await getCategory();
-  const { totalPost } = await getAllPost();
+  // const categories = await getCategory();
+  // const { totalPost } = await getAllPost();
   return (
     <>
-      <div className={styles.categoryContainer}>
+      {/* <div className={styles.categoryContainer}>
         {categories && totalPost && (
           <CategoryBar categories={categories} totalPost={totalPost} />
         )}
-      </div>
+      </div> */}
       <BackBtn color={"white"} />
       {children}
     </>
