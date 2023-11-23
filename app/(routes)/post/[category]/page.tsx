d54b7181d8dbf5ede39e042662898f287204ce92
22JH <toitoii080@gmail.com>
2023-11-23 17:57:55 +0900

    [FE] FIX : commit 초기화로 인해 수정된 url 다시 수정

--- app/(routes)/post/[category]/page.tsx
+++ app/(routes)/post/[category]/page.tsx
@@ -31,7 +31,7 @@ export default async function Post({ params, searchParams }: PropType) {
   //   Number(page)
   // );
   const res = await fetch(
-    `http://localhost:3000/api/post/${params.category}?page=${page}`,
+    `https://www.joo-dev.com/api/post/${params.category}?page=${page}`,
     { cache: "no-store" }
   );
 
