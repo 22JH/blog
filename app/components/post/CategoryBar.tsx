0b1b9064bacc53847d4cb598ccdae6f7ce2740ef
22JH <toitoii080@gmail.com>
2023-12-01 01:08:08 +0900

    [FE] UPDATE : 카테고리 accent조건 변경 (include -> useParams)

--- app/components/post/CategoryBar.tsx
+++ app/components/post/CategoryBar.tsx
@@ -3,16 +3,18 @@
 import Link from "next/link";
 import * as styles from "./CategoryBar.css";
 import { SelectOption } from "@/app/types/category.type";
-import { usePathname } from "next/navigation";
 import { assignInlineVars } from "@vanilla-extract/dynamic";
 import { vars } from "@/app/styles/themes/theme.css";
+import { useParams } from "next/navigation";
 interface PropType {
   categories: SelectOption[];
   totalPost: number;
 }
 
 export default function CategoryBar({ categories, totalPost }: PropType) {
-  const pathname = usePathname();
+  const params = useParams().category as string;
+  const categoryName = decodeURI(params);
+
   return (
     <ul className={styles.categories}>
       <li className={styles.category}>
@@ -21,9 +23,10 @@ export default function CategoryBar({ categories, totalPost }: PropType) {
           className={styles.categoryText}
           style={assignInlineVars({
             marginTop: "0",
-            [styles.categoryColor]: pathname.includes("/all")
-              ? vars.themeColor.buttonColor.activeColor
-              : vars.themeColor.buttonColor.notActiveColor,
+            [styles.categoryColor]:
+              categoryName === "all"
+                ? vars.themeColor.buttonColor.activeColor
+                : vars.themeColor.buttonColor.notActiveColor,
           })}
           key="all"
         >
@@ -41,9 +44,10 @@ export default function CategoryBar({ categories, totalPost }: PropType) {
               href={`/post/${category.label}?page=1`}
               className={styles.categoryText}
               style={assignInlineVars({
-                [styles.categoryColor]: pathname.includes(category.label)
-                  ? vars.themeColor.buttonColor.activeColor
-                  : vars.themeColor.buttonColor.notActiveColor,
+                [styles.categoryColor]:
+                  categoryName === category.label
+                    ? vars.themeColor.buttonColor.activeColor
+                    : vars.themeColor.buttonColor.notActiveColor,
               })}
             >{`${category.label} (${category.count})`}</Link>
           </li>
