8af83005b7ab46546aca63679debe018264dc747
22JH <toitoii080@gmail.com>
2023-11-30 18:46:23 +0900

    [FE] UPDATE : vh -> dvh로 변경

--- app/components/home/ThemeProvider.css.ts
+++ app/components/home/ThemeProvider.css.ts
@@ -5,5 +5,5 @@ export const themeContainer = style({
   backgroundColor: vars.themeColor.backgroundColor.bodyColor,
   width: "100%",
   height: "100%",
-  minHeight: "100vh",
+  minHeight: "100dvh",
 });
